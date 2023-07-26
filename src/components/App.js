627c69a91d40f454019095075d0d9568c1053d2b
Moses <mochengo29@gmail.com>
2023-07-26 15:20:56 +0300

    Completed assignment

--- src/components/App.js
+++ src/components/App.js
@@ -1,13 +1,24 @@
 import React from "react";
-import { Route, Switch } from "react-router-dom";
 import NavBar from "./NavBar";
 import Home from "./Home";
 import Actors from "./Actors";
 import Directors from "./Directors";
 import Movies from "./Movies";
 
+import { Routes, Route } from "react-router-dom";
+
 function App() {
-  return <div>{/*{code here}*/}</div>;
+  return (
+    <div>
+      <NavBar />
+      <Routes>
+        <Route path="/home" element={<Home />} />
+        <Route path="/movies" element={<Movies />} />
+        <Route path="/directors" element={<Directors />} />
+        <Route path="/actors" element={<Actors />} />
+      </Routes>
+    </div>
+  );
 }
 
 export default App;
--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -1,8 +1,27 @@
 import React from "react";
 import { NavLink } from "react-router-dom";
 
+const styled = {
+  fontSize: "20px",
+};
+
 function NavBar() {
-  return <div>{/*{code here}*/}</div>;
+  return (
+    <ul style={styled}>
+      <li>
+        <NavLink to="home">Home Page</NavLink>
+      </li>
+      <li>
+        <NavLink to="movies">Movies Page</NavLink>
+      </li>
+      <li>
+        <NavLink to="directors">Directories Page</NavLink>
+      </li>
+      <li>
+        <NavLink to="actors">Actors Page</NavLink>
+      </li>
+    </ul>
+  );
 }
 
 export default NavBar;
